17f108d6dc68dc2eeb4e44120a9567cb67469dd6
Derek Louis <derek.louis@avanade.com>
2021-10-08 12:50:34 -0400

    burger menu draft 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jess-birch';
+
+  burgerStatus: boolean = false;
+
+  toggleBurger() {
+    this.burgerStatus = !this.burgerStatus;
+  }
 }
